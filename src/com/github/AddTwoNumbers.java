69aab5def815acc2993e3723335d67701e7fcff1
Ankam Suresh <ankamsuresh21@gmail.com>
2023-11-03 12:23:37 +0530

    code modefied

--- src/com/github/AddTwoNumbers.java
+++ src/com/github/AddTwoNumbers.java
@@ -2,8 +2,9 @@ package com.github;
 
 public class AddTwoNumbers {
 	public static void main(String[] args) {
-		int a = 5, b = 3;
-		System.out.println(a + b);
+		int a = 5, b = 3, c;
+		c = a + b;
+		System.out.println(c);
 	}
 
 }
